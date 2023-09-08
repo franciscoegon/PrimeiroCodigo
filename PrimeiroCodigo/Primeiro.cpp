44bee2d2e9a57d295e621108d6321aef5afb5839
danie <danie@DESKTOP-R861260>
2023-09-07 23:15:58 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroCodigo/Primeiro.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Nome: Francisco Eduardo Goncalves" << std::endl;
+	std::cout << "idade: 20 anos" << std::endl;
+	std::cout << "O Gremio e o maior do sul" << std::endl;
+	system("PAUSE");
+	return 0;
+}
